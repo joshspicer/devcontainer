bf5cb8f25a879a8f8a0d39e7d93f356708159cc6
Josh Spicer <hello@joshspicer.com>
2021-09-17 18:56:06 -0400

    example

---
+++ install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+
+set -a
+. /tmp/build-features/features.env
+set +a
+
+mkdir -p /usr/local/numbers
+
+if [ ! -z ${_JOSHSPICER_DEVCONTAINER_ONE} ]; then
+        bash ./one.sh
+fi
+
+if [ ! -z ${_JOSHSPICER_DEVCONTAINER_TWO} ]; then
+        bash ./two.sh
+fi
+
+if [ ! -z ${_JOSHSPICER_DEVCONTAINER_THREE} ]; then
+        bash ./three.sh
+fi
---
+++ three.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+touch /usr/local/numbers/three
